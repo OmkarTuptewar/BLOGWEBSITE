90c15e783f15b2a6adaf1d67ee7f4ea734bac704
slyjain <jain.samyak.3210@gmail.com>
2024-07-29 04:13:17 +0000

    Added Cleanup functions in useEffect to add debouncing effect

--- src/App.jsx
+++ src/App.jsx
@@ -4,40 +4,58 @@ import './App.css'
 
 function App() {
   console.log('App rendered')
-  const [loggedIn,setLoggedIn] = useState(false);
+  const [loggedIn, setLoggedIn] = useState(false);
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  
-  
+
+  const [isFormValid, setIsFormValid] = useState(false);
   // if(storedLogin){
   //   setLoggedIn(true);
   // }
   //this is wrong because it will cause an infinite loop, that's why we use useEffect
   //side effect is something that happens outside of the component
-  useEffect(()=>{
+  useEffect(() => {
     const storedLogin = localStorage.getItem('loggedIn')
-    if(storedLogin=="true"){
+    if (storedLogin == "true") {
       setLoggedIn(true);
     }
-  },[])
+  }, [])
+  var timeout;
+  useEffect(() => {
+   
+    
+     const timeout = setTimeout(() => {
+      
+      console.log('checking form validity and debouncing')
+    if (email.length > 5 && password.length > 5) {
+      setIsFormValid(true)
+    } else {
+      setIsFormValid(false)
+    }},2000)
+    return () => {
+      clearTimeout(timeout)
+    }
+  }, [email, password])
   const handleSubmit = (e) => {
     e.preventDefault()
-    console.log(email, password)  
+    console.log(email, password)
     console.log('submit')
     setLoggedIn(true)
-    localStorage.setItem('loggedIn',true)
+    localStorage.setItem('loggedIn', true)
   }
   return (
     <>
-    <div className='formSection'>{
-    loggedIn ?<div> Welcome<button onClick={()=>{setLoggedIn(false);
-    localStorage.removeItem("loggedIn")}}> LogOut</button></div> :
-    <form onSubmit={handleSubmit} className='Form'>
-     <div> <input type="text" className='emailInput' placeholder='Email' value={email} onChange={(e)=>setEmail(e.target.value)}/></div>
-     <div><input type="password" className="password" placeholder='Password' value={password} onChange={(e)=>setPassword(e.target.value)}/></div>
-     <div><button type='submit' className={(email.length>5&&password.length>5)?"enabled":"disabled"} >Submit</button></div>
-    </form>}
-    </div>
+      <div className='formSection'>{
+        loggedIn ? <div> Welcome<button onClick={() => {
+          setLoggedIn(false);
+          localStorage.removeItem("loggedIn")
+        }}> LogOut</button></div> :
+          <form onSubmit={handleSubmit} className='Form'>
+            <div> <input type="text" className='emailInput' placeholder='Email' value={email} onChange={(e) => setEmail(e.target.value)} /></div>
+            <div><input type="password" className="password" placeholder='Password' value={password} onChange={(e) => setPassword(e.target.value)} /></div>
+            <div><button type='submit' className={(isFormValid) ? "enabled" : "disabled"} >Submit</button></div>
+          </form>}
+      </div>
     </>
   )
 }
